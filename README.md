de0c7789ed976cf56f96684f125fc1669c083a03
Sergio Alvarez-Napagao <tranchis@gmail.com>
2013-06-28 13:33:23 +0200

    initial commit of the project

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-ribasim
-=======
+RiverBaSim
+==========
 
 Water Basin Management Simulator, Repast-based
+
+Software needed
+---------------
+
+* Repast Simphony 2.0
+* 
---
+++ RiverBaSim/src/com/github/kemlg/riverbasim/factories/ReLogoPanelCreator.java
@@ -0,0 +1,15 @@
+package com.github.kemlg.riverbasim.factories;
+
+import javax.swing.JPanel;
+import repast.simphony.relogo.factories.AbstractReLogoPanelCreator;
+import com.github.kemlg.riverbasim.relogo.UserGlobalsAndPanelFactory;
+
+public class ReLogoPanelCreator extends AbstractReLogoPanelCreator {
+
+	public void addComponents(JPanel parent) {
+		UserGlobalsAndPanelFactory ugpf = new UserGlobalsAndPanelFactory();
+		ugpf.initialize(parent);
+		ugpf.addGlobalsAndPanelComponents();
+	}
+
+}
