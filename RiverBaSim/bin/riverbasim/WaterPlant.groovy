78d5e46268d4b93588dd3507b5e8b2db301567c9
Luis Oliva (UPC) <loliva@lsi.upc.edu>
2013-07-18 00:55:13 +0200

    RiverBaSim: River section agent created

--- RiverBaSim/bin/riverbasim/WaterPlant.groovy
+++ RiverBaSim/bin/riverbasim/WaterPlant.groovy
@@ -138,7 +138,6 @@ public class WaterPlant  {
             // Cleans N units of wastewater
             callSomething();
             // Send the treated water to be dumped to the river.
-            Edit Using Wizard Dialog...
 
         } else  {
 
